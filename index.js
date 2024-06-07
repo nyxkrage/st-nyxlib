82b5e10dd66d7322b9e1a0c25d9051016e0286e6
Carsten Kragelund <carsten@kragelund.me>
2024-06-07 08:06:47 +0000

    fix extra import

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { compile } from "./jq.js"
 import { SlashCommandParser } from '../../../slash-commands/SlashCommandParser.js';
 import { SlashCommand } from '../../../slash-commands/SlashCommand.js';
-import { getContext, isTrueBoolean } from '../../../extensions.js';
+import { getContext } from '../../../extensions.js';
 import { isTrueBoolean } from '../../../utils.js';
 
 import { ARGUMENT_TYPE, SlashCommandArgument, SlashCommandNamedArgument } from '../../../slash-commands/SlashCommandArgument.js';
